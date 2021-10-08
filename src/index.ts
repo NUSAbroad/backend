3313d55f6cf7ff9e7053bdb3a1af4e3561333fec
zatkiller <gheedylan@yahoo.com.sg>
2021-10-08 21:40:09 +0800

    Test CI

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', async (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('Hello World!!!');
 });
 
 app.listen(PORT, () => {
