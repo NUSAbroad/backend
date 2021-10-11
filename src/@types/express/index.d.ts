5140d2df98d60435972d5f2317347f94d5fa96d9
Dylan Ghee <59414467+zatkiller@users.noreply.github.com>
2021-10-11 15:07:23 +0800

    Add University CSV uploader (#14)
    * Add University CSV uploader
    
    * Add endpoint to clear university entries
    
    * Remove comment

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -3,6 +3,8 @@ import { Module, University, Mapping } from '../../models';
 declare module 'express' {
   interface Request {
     university?: University;
+
+    file?: Express.Multer.File;
     module?: Module;
     mapping?: Mapping;
   }
--- src/routes/universities.ts
+++ src/routes/universities.ts
@@ -6,6 +6,8 @@ const router = Router();
 router.get('/', universityController.indexUniversityFuncs);
 router.get('/:id', universityController.showUniversityFuncs);
 router.post('/', universityController.createUniversityFuncs);
+router.post('/import', universityController.importUniversityFuncs);
+router.post('/reset', universityController.resetUniversityFuncs);
 router.put('/:id', universityController.updateUniversityFuncs);
 router.delete('/:id', universityController.destroyUniversityFuncs);
 
