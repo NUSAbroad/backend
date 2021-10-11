d8c49af567e91d8acbec35ad6200feb6994fd87f
zatkiller <gheedylan@yahoo.com.sg>
2021-10-11 20:16:35 +0800

    Add module populate endpoint

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -5,4 +5,6 @@ dotenv.config();
 const PORT = process.env.PORT;
 const DATABASE_URL: string = process.env.DATABASE_URL || '';
 
-export { PORT, DATABASE_URL };
+const NUS_MODS_MODULES_INFO_URL = 'https://api.nusmods.com/v2/';
+
+export { PORT, DATABASE_URL, NUS_MODS_MODULES_INFO_URL };
--- src/controllers/moduleController.ts
+++ src/controllers/moduleController.ts
@@ -1,6 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
-import { Module } from '../models';
-import { NotFound } from 'http-errors';
+import { Module, University } from '../models';
+import { NotFound, BadRequest } from 'http-errors';
+import axios, { AxiosResponse } from 'axios';
+import { NUS_MODS_MODULES_INFO_URL } from '../consts';
+import { ModuleInfo, formatModules } from '../utils/modules';
+import { ModuleCreationAttributes } from '../models/Module';
 
 async function retrieveModule(req: Request, res: Response, next: NextFunction) {
   try {
@@ -20,7 +24,8 @@ async function retrieveModule(req: Request, res: Response, next: NextFunction) {
 async function indexModule(req: Request, res: Response, next: NextFunction) {
   try {
     const modules = await Module.findAll({
-      order: [['id', 'ASC']]
+      order: [['id', 'ASC']],
+      attributes: { exclude: ['createdAt', 'updatedAt'] }
     });
 
     res.status(200).json(modules);
@@ -64,8 +69,51 @@ async function destroyModule(req: Request, res: Response, next: NextFunction) {
   }
 }
 
+async function resetModule(req: Request, res: Response, next: NextFunction) {
+  try {
+    await Module.destroy({
+      where: {}
+    });
+    res.status(200).end();
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function populateModule(req: Request, res: Response, next: NextFunction) {
+  try {
+    if (!req.body.year) throw new BadRequest('Not academic year specified');
+
+    const response: AxiosResponse = await axios.get(
+      NUS_MODS_MODULES_INFO_URL + `/${req.body.year}/moduleInfo.json`
+    );
+
+    const modulesInfo: ModuleInfo[] = response.data;
+
+    const nusUniversity = await University.findOne({
+      where: {
+        name: 'National University of Singapore'
+      }
+    });
+
+    if (!nusUniversity) throw new BadRequest('NUS University not found!');
+
+    const nusId = nusUniversity.id;
+
+    const formattedModules: ModuleCreationAttributes[] = formatModules(modulesInfo, nusId);
+
+    const modules = await Module.bulkCreate(formattedModules);
+
+    res.status(201).json(modules);
+  } catch (err) {
+    next(err);
+  }
+}
+
 export const indexModuleFuncs = [indexModule];
 export const showModuleFuncs = [retrieveModule, showModule];
 export const createModuleFuncs = [createModule];
 export const updateModuleFuncs = [retrieveModule, updateModule];
 export const destroyModuleFuncs = [retrieveModule, destroyModule];
+export const populateModuleFuncs = [populateModule];
+export const resetModuleFuncs = [resetModule];
