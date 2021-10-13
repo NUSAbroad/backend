e5250d310783edad592b830dabd3c19fe9553423
zatkiller <gheedylan@yahoo.com.sg>
2021-10-14 01:43:56 +0800

    Hot fix for reset module endpoint

--- src/routes/modules.ts
+++ src/routes/modules.ts
@@ -7,7 +7,7 @@ router.get('/', moduleController.indexModuleFuncs);
 router.get('/:id', moduleController.showModuleFuncs);
 router.post('/', moduleController.createModuleFuncs);
 router.post('/populate', moduleController.populateModuleFuncs);
-router.post('/reset', moduleController.createModuleFuncs);
+router.post('/reset', moduleController.resetModuleFuncs);
 router.put('/:id', moduleController.updateModuleFuncs);
 router.delete('/:id', moduleController.destroyModuleFuncs);
 
