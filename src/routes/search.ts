a263a2bd35ffed4917d727dbbb0d5db1dea4bc73
zatkiller <gheedylan@yahoo.com.sg>
2021-10-17 14:37:15 +0800

    Add new search routes

--- src/routes/search.ts
+++ src/routes/search.ts
@@ -3,7 +3,13 @@ import * as searchController from '../controllers/searchController';
 
 const router = Router();
 
-router.get('/', searchController.searchFuncs);
-router.get('/:query', searchController.searchFuncs);
+router.get('/general', searchController.searchFuncs);
+router.get('/general/:query', searchController.searchFuncs);
+router.get('/faculty/', searchController.searchFacultiesFuncs);
+router.get('/faculty/:query', searchController.searchFacultiesFuncs);
+router.get('/moduleName', searchController.searchModuleNameFuncs);
+router.get('/moduleName/:query', searchController.searchModuleNameFuncs);
+router.get('/moduleCode', searchController.searchModuleCodeFuncs);
+router.get('/moduleCode/:query', searchController.searchModuleCodeFuncs);
 
 export default router;
