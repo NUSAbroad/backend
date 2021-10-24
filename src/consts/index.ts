db353045457f158174e4644c5a05256df94138ce
zatkiller <gheedylan@yahoo.com.sg>
2021-10-24 17:42:13 +0800

    Add Redis and improve search

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -8,9 +8,19 @@ const DATABASE_URL: string = process.env.DATABASE_URL || '';
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const SECRET_KEY = process.env.SECRET_KEY || '';
 const ON_AUTH = process.env.ON_AUTH === 'true' || false;
+const REDIS_URL = process.env.REDIS_URL || '';
 
 const NUS_MODS_MODULES_INFO_URL = 'https://api.nusmods.com/v2/';
 const NUS = 'National University of Singapore';
 const NUSSLUG = slug(NUS);
 
-export { PORT, DATABASE_URL, NUS_MODS_MODULES_INFO_URL, NUSSLUG, NODE_ENV, SECRET_KEY, ON_AUTH };
+export {
+  PORT,
+  DATABASE_URL,
+  NUS_MODS_MODULES_INFO_URL,
+  NUSSLUG,
+  NODE_ENV,
+  SECRET_KEY,
+  ON_AUTH,
+  REDIS_URL
+};
